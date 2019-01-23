f0b1b3322b7d8ee1be3f366422f02ce0c19b0af3
krishnakartheek <35799930+krishnakartheek@users.noreply.github.com>
2019-01-23 23:49:40 +0530

    Add files via upload

---
+++ tournamentpalyers.php
@@ -0,0 +1,26 @@
+<?php
+namespace SDM\RPS;
+class Player
+{
+    /**
+     * @var string
+     */
+    private $name;
+    /**
+     * @var string
+     */
+    private $hand;
+    public function __construct(string $name, string $hand)
+    {
+        $this->name = $name;
+        $this->hand = $hand;
+    }
+    public function getHand(): string
+    {
+        return $this->hand;
+    }
+    public function getName(): string
+    {
+        return $this->name;
+    }
+}
