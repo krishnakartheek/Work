cfc60049232b594d88e11cef35c2839d86e9e220
krishnakartheek <35799930+krishnakartheek@users.noreply.github.com>
2019-01-23 23:55:39 +0530

    Update tournament.php

--- tournament.php
+++ tournament.php
@@ -1,3 +1,22 @@
+Skip to content
+ 
+Search or jump to…
+
+Pull requests
+Issues
+Marketplace
+Explore
+ @krishnakartheek Sign out
+1
+0 13 tanmoy1981/gray_owl-1
+forked from scandesignsdk/gray_owl
+ Code  Pull requests 0  Projects 0  Wiki  Insights
+gray_owl-1/src/RPS/RPSTournament.php
+e29637a  on Jun 23, 2017
+@tanmoy1981 tanmoy1981 added changes
+@tanmoy1981 @lsv
+     
+105 lines (93 sloc)  2.64 KB
 <?php
 namespace SDM\RPS;
 class RPSTournament
@@ -17,14 +36,77 @@ class RPSTournament
         $this->players = $players;
     }
     /**
-     * Get the winner of the tournament.
+     * Get the winner of the tournament
      *
      * @throws InvalidTournamentException
      * @throws CancelledTournamentException
      *
      * @return Player
      */
-    public function getWinner(): Player
+    public function getWinner() : Player
+    {   
+        foreach ($this->players as $single_player) {
+            if(!in_array(strtoupper($single_player->getHand()), array(self::ROCK, self::PAPER, self::SCISSOR))) {
+                throw new InvalidTournamentException();
+                break;
+            }
+        }
+        if(count($this->players) <= 1) {
+            throw new CancelledTournamentException();
+        }
+        else {
+            // Start rounds
+            return $this->round_plays($this->players);
+        }            
+    }
+    /**
+     * Conduct rounds of the tournament
+     * @param Player[] $players Array with players
+     *
+     * @return Player
+     */
+    public function round_plays(array $players) : Player
     {
+        $winner_arr = [];
+        if(count($players) == 1) {
+            // Winner here.
+            return $players[0];
+        }
+        else {
+            for($i = 0; $i < (count($players) - 1); $i+=2 ) {
+                $winner_arr[] = $this->each_play($players[$i], $players[$i+1]);  
+            }
+            if(count($players) % 2) {
+                // walk over for last single player.
+                $winner_arr[] = $players[count($players) - 1]; 
+            }
+            return $this->round_plays($winner_arr);
+        }
+    }
+    /**
+     * Conduct each play between 2 players and return winner.
+     * @param Player $player1
+     * @param Player $player2     
+     *
+     * @return Player
+     */
+    public function each_play($player1, $player2) : Player
+    {
+        $win_combination = array(
+                                    array(self::ROCK, self::SCISSOR),
+                                    array(self::SCISSOR, self::PAPER),
+                                    array(self::PAPER, self::ROCK)
+                                );
+        $p1_hand = strtoupper($player1->getHand());
+        $p2_hand = strtoupper($player2->getHand());
+        if($p1_hand == $p2_hand){
+            return $player1;
+        }
+        if(in_array(array($p1_hand, $p2_hand), $win_combination)) {
+           return $player1; 
+        }
+        else {
+            return $player2;
+        }   
     }
 }
